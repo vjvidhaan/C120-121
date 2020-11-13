cea8ca8ab81b70a9c7d7d732c3843934ed079955
vjvidhaan <73320199+vjvidhaan@users.noreply.github.com>
2020-11-12 16:43:57 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier("MobileNet",modelLoaded);
+}
+
+
+function preload(){
+
+}
+
+function draw(){
+  image(video, 0, 0, 400, 300);
+  classifier.classify(video, gotResults);
+}
+
+function modelLoaded(){
+  console.log("modelLoaded!");
+}
+
+function gotResults(error,results){
+  if(error){
+  console.error(error);
+  }
+  else{
+      console.log(results);
+      document.getElementById("object_answer").innerHTML = results[0].label;
+      document.getElementById("Acuracy_answer").innerHTML = results[0].confidence.toFixed(3);
+         
+  }
+  }
+  
